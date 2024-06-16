ab36b453e0227776ebfc7a207ec926a21ebe6014
AI_Nomads <chugue85@gmail.com>
2024-06-16 14:45:19 +0900

    ready for deploy

--- src/main/java/com/example/finalproject/domain/MainController.java
+++ src/main/java/com/example/finalproject/domain/MainController.java
@@ -4,11 +4,17 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class MainController {
     private final HttpServletRequest request;
     private final HttpSession session;
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok";
+    }
 
 }
